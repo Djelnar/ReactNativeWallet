dcd421fb7bd0a4bcdbf215b32842e957b57a4aeb
Elnar Ibragimov <djelnar@gmail.com>
2024-03-17 02:21:28 +0200

    Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+const {configure, presets} = require('eslint-kit');
+
+module.exports = configure({
+  root: __dirname,
+  allowDebug: process.env.NODE_ENV !== 'production',
+  presets: [
+    presets.imports(),
+    presets.node(),
+    presets.react(),
+    presets.prettier(),
+    presets.typescript(),
+  ],
+  extend: {
+    extends: ['@react-native'],
+    ignorePatterns: ['!**/*', 'node_modules'],
+    rules: {
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+    },
+  },
+});
---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text>123</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
